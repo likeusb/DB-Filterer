573216f9d4a8abbc8d8a8c98bf014d5aa7017828
likeusb <80967503+likeusb@users.noreply.github.com>
2024-08-17 10:28:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getCookie(name) {
     if (parts.length == 2) return parts.pop().split(";").shift();
 };
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
 
 function limitedInsert() {
     for (var i = 1; i <= numbers.length / 10; i++) {
@@ -39,25 +39,47 @@ function resetSequential() {
 };
 
 usedRandom = [];
+// I know this is extra steps for no reason but if you remove this, it will start to shuffle the numbers array, not the numbersforShuffle array. I don't know why. Lord save us all.
+var numbersforShuffle = numbers.toString().split(',');
+shuffle(numbersforShuffle);
 
 function randomInsert() {
-    shuffle(numbers);
-    usedRandom.push(numbers[0]);
-    if (usedRandom.length > numbers.length) {
-        usedRandom = [];
+    // usedRandom.push(numbersforShuffle[0]);
+    // if (usedRandom.length > numbers.length) {
+    //     usedRandom = [];
+    // }
+    
+    // if (numbersforShuffle.length == 1) {
+    //     numbersforShuffle = numbers.toString().split(',');
+    // }
+    
+    // for (let i = 0; i < numbers.length; i++) {
+    //     if (numbersforShuffle[0] == numbers[i]) {
+    //         numbersforShuffle.splice(0, 1);
+    //         break;
+    //     }
+    // }
+    // console.log(numbersforShuffle[0])
+    
+    if (numbersforShuffle.length == 1) {
+        shuffle(numbersforShuffle);
     };
-    console.log(usedRandom);
-    console.log(numbers[0]);
-    console.log(numbers.indexOf(23))
+
+    usedRandom.push(numbersforShuffle[i]);
+    i++;
+
+    if (i > numbers.length) {
+        i = 0;
+    };
 };
 
 // Credit to @Álvaro González on StackOverflow. Link: https://stackoverflow.com/a/59837259
 function shuffle(arr) {
-    var i = arr.length, j, temp;
-    while(--i > 0){
-      j = Math.floor(Math.random()*(i+1));
+    var k = arr.length, j, temp;
+    while(--k > 0){
+      j = Math.floor(Math.random()*(k+1));
       temp = arr[j];
-      arr[j] = arr[i];
-      arr[i] = temp;
+      arr[j] = arr[k];
+      arr[k] = temp;
     }
 }
