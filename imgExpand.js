3d643f781138994b616fcfc6e3a7c4dcd5b9cd12
likeusb <80967503+likeusb@users.noreply.github.com>
2024-08-17 19:48:50 +0300

    Add files via upload

--- imgExpand.js
+++ imgExpand.js
@@ -1,29 +1,12 @@
-// document.getElementById('shrink').addEventListener('click', () => {
-//     document.getElementsByClassName('imgContainer')[0].classList.remove('active');
-//     document.getElementsByClassName('imgContainer')[0].classList.add('inactive');
-// });
-
-// document.getElementById('img').addEventListener('click', () => {
-//     document.getElementsByClassName('imgContainer')[0].classList.remove('inactive');
-//     document.getElementsByClassName('imgContainer')[0].classList.add('active');
-// });
-
-// document.getElementById('download').addEventListener('click', () => {
-//     var link = document.createElement("a");
-//     link.download = 'SLR.jpg';
-//     link.href = 'http://127.0.0.1:5500/img/SLR.jpg';
-//     link.click();
-// });
-
 var imgs = document.getElementsByClassName('img');
 var imgContainers = document.getElementsByClassName('imgContainer');
 var shrink = document.getElementsByClassName('shrink');
 
-// Expansion and shrinking
 for (var i = 0; i < imgs.length; i++) {
     imgs[i].addEventListener('click', function() {
         this.parentElement.classList.remove('inactive');
         this.parentElement.classList.add('active');
+        previous();
     });
 
     shrink[i].addEventListener('click', function() {
@@ -31,8 +14,6 @@ for (var i = 0; i < imgs.length; i++) {
         this.parentElement.parentElement.classList.add('inactive');
     });
 };
-
-// Downloading
 
 var download = document.getElementsByClassName('download');
 for (var i = 0; i < download.length; i++) {
@@ -43,17 +24,26 @@ for (var i = 0; i < download.length; i++) {
 
 function downloadImage(element) {
     regexFilter(element.parentElement.parentElement.innerHTML);
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = src;
+    link.click();
 };
 
 var imgSrc = '';
 var imgFilename = '';
 
 function regexFilter(string) {
-    console.log(string)
     const regex = /src=".*?"/g;
-    console.log(regex);
     const found = string.match(regex)[0];
-    console.log(found);
+
+    src = found.replace('src="', '').replace('"', '');
+
+    const namePre = src.split('/');
+    name = namePre[namePre.length - 1];
 };
 
-console.log(imgContainers[0].innerHTML);
+function previous() {
+    console.log(imgContainers);
+    console.log(imgContainers.indexOf(this));
+}
